72092c65b930acdce77f001ca17cb4561ea31754
denikn <denikurnianto@mail.ugm.ac.id>
2021-12-08 10:32:24 +0700

    Remove test endpoint on route

--- routes/web.php
+++ routes/web.php
@@ -38,7 +38,6 @@ $router->group(['prefix' => 'api'], function () use ($router)
 	{
 		$router->post('checkout', 'CheckoutController@store');
 		$router->get('checkout/{checkout_id}', 'CheckoutController@show');
-		$router->get('xxx/{checkout_id}', 'CheckoutController@checkExpiredCheckout');
 		$router->post('payment', 'PaymentController@store');
 		$router->post('validation', 'PaymentController@paymentValidation');
 	});
